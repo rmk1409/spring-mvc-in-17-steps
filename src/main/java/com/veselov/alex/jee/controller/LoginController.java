a632817d4cc6c8ec10ceb3e5c81f098975614cf9
Pogorelov Roman <rmk140987@gmail.com>
2019-12-08 16:45:12 +0300

    Spring MVC - Add log4j

--- src/main/java/com/veselov/alex/jee/controller/LoginController.java
+++ src/main/java/com/veselov/alex/jee/controller/LoginController.java
@@ -1,17 +1,17 @@
 package com.veselov.alex.jee.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String login(){
         return "login";
     }
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     @ResponseBody
     public String sayHello(){
         return "Hello World";
