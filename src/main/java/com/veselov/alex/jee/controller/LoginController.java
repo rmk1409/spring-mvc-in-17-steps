d73a75337c81c77d7a59fad24d15c5240958743f
Pogorelov Roman <rmk140987@gmail.com>
2019-12-08 16:29:08 +0300

    Spring MVC - Add ViewResolver example.

--- src/main/java/com/veselov/alex/jee/controller/LoginController.java
+++ src/main/java/com/veselov/alex/jee/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.veselov.alex.jee;
+package com.veselov.alex.jee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
     @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/hello")
     @ResponseBody
     public String sayHello(){
         return "Hello World";
