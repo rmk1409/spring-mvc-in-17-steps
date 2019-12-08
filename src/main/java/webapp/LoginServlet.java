49bd70db1c9e9502025c3ef1f38db79523ad6a38
Pogorelov Roman <rmk140987@gmail.com>
2019-12-08 14:43:20 +0300

    Add post method

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -1,20 +1,18 @@
 package webapp;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /*
  * Browser sends Http Request to Web Server
- * 
+ *
  * Code in Web Server => Input:HttpRequest, Output: HttpResponse
  * JEE with Servlets
- * 
+ *
  * Web Server responds with Http Response
  */
 
@@ -33,11 +31,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String param = request.getParameter("name");
-		request.setAttribute("name", param);
-		request.getRequestDispatcher("/WEB-INF//views/login.jsp").forward(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("name", request.getParameter("name"));
+        request.setAttribute("password", request.getParameter("password"));
+        request.getRequestDispatcher("/WEB-INF//views/login.jsp").forward(request, response);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("name", request.getParameter("name"));
+        request.setAttribute("password", request.getParameter("password"));
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+    }
 }
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -2,19 +2,16 @@
   Created by IntelliJ IDEA.
   User: roman.pogorelov
   Date: 08.12.2019
-  Time: 12:12
+  Time: 14:41
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Login</title>
+    <title>Welcome</title>
 </head>
 <body>
-<form action="/login.do" method="post">
-    Enter your name <input type="text" name="name"/>
-    Enter your password <input type="password" name="password"/>
-    <input type="submit"/>
-</form>
+    ${name}
+    ${password}
 </body>
 </html>
